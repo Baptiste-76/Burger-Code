587dd5dc0e444594e404926f98c4944e6e7ec655
Baptiste Bidaux <baptistelise@orange.fr>
2020-05-07 16:16:34 +0200

    Connexion aà la BDD

--- admin/database.php
+++ admin/database.php
@@ -1,15 +1,11 @@
 <?php
 
 class Database {
-    private static $dbHost = "localhost";
-    private static $dbName = "burger_code";
-    private static $dbUser = "root";
-    private static $dbUserPassword = "";
     private static $connection = null;
     
     public static function connect() {     
         try {
-            self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUser, self::$dbUserPassword);
+            self::$connection = new PDO("mysql:host=" . $_ENV['HOST'] . ";dbname=" .$_ENV ['DB_NAME'], $_ENV['USER'], $_ENV['PASSWORD']);
         } catch (PDOException $exception) {
             die($exception->getMessage());
         }
