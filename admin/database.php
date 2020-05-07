f02411125f91316c3e4ec8ef4413d4567703e0bd
Baptiste Bidaux <baptistelise@orange.fr>
2020-05-07 16:27:46 +0200

    Connexion aà la BDD

--- admin/database.php
+++ admin/database.php
@@ -5,7 +5,8 @@ class Database {
     
     public static function connect() {     
         try {
-            self::$connection = new PDO("mysql:host=" . $_ENV['HOST'] . ";port=3306;dbname=" .$_ENV ['DB_NAME'], $_ENV['USER'], $_ENV['PASSWORD']);
+            // self::$connection = new PDO("mysql:host=" . $_ENV['HOST'] . ";port=3306;dbname=" .$_ENV ['DB_NAME'], $_ENV['USER'], $_ENV['PASSWORD']);
+            self::$connection = new PDO($_ENV['JAWSDB_URL']);
         } catch (PDOException $exception) {
             die($exception->getMessage());
         }
